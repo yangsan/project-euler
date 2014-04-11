1eb5dcb2c4498cc76d041761cfcc6b95e465a8c2
Kevin Young <kevinyounglogin@163.com>
2014-04-11 19:27:08 +0800

    rename problem1

--- prob1.py
+++ prob1.py
@@ -1,4 +1,10 @@
 # -*- coding: utf8 -*-
+"""
+Problem 1:
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 
 def multiples1(n):
@@ -21,5 +27,14 @@ def multiples2(n):
             sum += i
     return sum
 
+
+def multiples3(n):
+    """
+    With generator only use O(1) memory.
+    """
+    for i in range(1, n):
+        if i % 3 == 0 or i % 5 == 0:
+            yield i
+
 if __name__ == "__main__":
-    print multiples2(1000)
+    print sum(multiples3(1000))
