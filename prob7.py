a09877f40f738e239addf8af8d3cfb95db5609d5
Kevin Young <kevinyounglogin@163.com>
2014-04-12 22:27:53 +0800

    add a new version of isPrime to check if a number is prime using a prime list as candidates. Over twice as fast, although using more memory.

--- prob7.py
+++ prob7.py
@@ -7,7 +7,8 @@ What is the 10 001st prime numberl?
 import math
 
 
-def isPrime(n):
+# version 1
+def isPrime1(n):
     """
     Check if n is a prime number.
     """
@@ -19,18 +20,47 @@ def isPrime(n):
     return True
 
 
-def findprime(nth):
+def findprime1(nth):
     """
     Find the nth prime number, begin with 2.
     """
     n = 2
     primelist = []
     while True:
-        if isPrime(n):
+        if isPrime1(n):
             primelist.append(n)
         if len(primelist) == nth:
             return primelist[-1]
         n += 1
 
+
+#version 2
+def isPrime2(n, primelist):
+    """
+    n is the number to be checked, prime is a primelist generated whose
+    elementsare all smaller than n.
+    """
+    if n <= 1:
+        return False
+    sqrtofn = int(math.sqrt(n))
+    for i in primelist:
+        if i > sqrtofn:
+            break
+        if n % i == 0:
+            return False
+    return True
+
+
+def findprime2(nth):
+    primelist = []
+    n = 2
+    while True:
+        if isPrime2(n, primelist):
+            primelist.append(n)
+        if len(primelist) == nth:
+            return primelist[-1]
+        n += 1
+
+
 if __name__ == "__main__":
-    print findprime(10001)
+    print findprime2(10001)
