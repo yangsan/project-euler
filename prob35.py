a4d551f84c03f5762bf4b63aa04efced258a5f80
kevin <kevinyounglogin@163.com>
2014-04-16 16:21:48 +0800

    完成了代码，但答案不正确，再找找问题在哪儿

--- prob35.py
+++ prob35.py
@@ -1,15 +1,13 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
-The number, 197, is called a circular prime because all rotations of the digits:
-197, 971, and 719, are themselves prime.There are thirteen such primes below
+The number, 197, is called a circular prime because all rotations of the digits
+:197, 971, and 719, are themselves prime.There are thirteen such primes below
 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.How many circular
 primes are there below one million?
 """
 
-import math
 
-
-def primegen1(upperbuond):
+def prime_gen_1(upperbuond):
     """
     A generator to produce a prime number sequence.
     """
@@ -27,25 +25,54 @@ def primegen1(upperbuond):
     return primenumber
 
 
-def primegen2(upperbound):
+def prime_gen_2(upperbound):
     """
     A generator to produce a prime number list using Sieve of Eratosthenes.
+    Return a list of prime number under upperbound.
     """
-    number = range(2,upperbound)
-    boolean = [1 for i in range(2,upperbound)]
+    number = range(2, upperbound)
+    boolean = [1 for i in range(2, upperbound)]
 
     for i, item in enumerate(number):
         if boolean[i]:
-            #print i,item,boolean[i]
             while i + item < len(number):
                 i += item
                 boolean[i] = 0
 
-    prime = [a for i, a in enumerate(number) if boolean[i]]
-    return prime
+    return [a for i, a in enumerate(number) if boolean[i]]
 
 
+def circular_of_a_number(number):
+    number = str(number)
+    CircularOfNumber = []
+    for i in range(len(number) - 1):
+        number = number[1:] + number[0]
+        CircularOfNumber.append(int(number))
+    return CircularOfNumber
+
+
+def number_of_circular_primes_under(n):
+    CircularPrimes = []
+    PrimeNumbers = prime_gen_2(n)
+    #print "Got the prime list."
+    #for prime in PrimeNumbers:
+    for i, prime in enumerate(PrimeNumbers):
+        if prime in CircularPrimes:
+            continue
+        else:
+            CircularOfNumber = circular_of_a_number(prime)
+            flag = True
+            for number in CircularOfNumber:
+                if number not in PrimeNumbers:
+                    flag = False
+                    break
+            if flag:
+                CircularOfNumber.append(prime)
+                CircularPrimes += CircularOfNumber
+        #print i/float(len(PrimeNumbers))
+
+    print CircularPrimes
+    print len(CircularPrimes)
 
 if __name__ == "__main__":
-    print primegen1(100)
-    print primegen2(100)
+    number_of_circular_primes_under(1000000)
